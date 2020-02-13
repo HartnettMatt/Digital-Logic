ceee267c641a29b0a0512879038a037acc686341
Matt Hartnett <matthew.e.hartnett@gmail.com>
2020-02-13 08:48:03 -0700

    Added final comments to design 1

--- Project 1/design1.v
+++ Project 1/design1.v
@@ -1,5 +1,17 @@
-module design1 (input [7:0] switch, input [1:0] key, output reg [9:0] leds,output [7:0] hex0,output [7:0] hex1,output [7:0] hex2,output [7:0] hex3,output [7:0] hex4,output [7:0] hex5);
+//The purpose of this design is to have LEDR[7:0] to be controlled by SW[7:0], and have KEY[0] invert the values. Additionally, HEX[5:0] are used to display lab partners birthday, and when KEY[1] is high, change to my birthday
+module design1 (switch,key,leds,hex0,hex1,hex2,hex3,hex4,hex5);
 
+input [7:0] switch;
+input [1:0] key;
+output reg [9:0] leds;
+output [7:0] hex0;
+output [7:0] hex1;
+output [7:0] hex2;
+output [7:0] hex3;
+output [7:0] hex4;
+output [7:0] hex5;
+
+//Light the leds based on the switch positions, and invert the leds when key0 is high
 always @(key[0])
 begin
   if(key[0])
@@ -12,17 +24,19 @@ begin
   end
 end
 
-//Displays that are the same between our birthdays
+//seg2 is a reg for the display that needs to be changed
 reg [3:0] seg2 = 4'b0011;
+sevenSeg S2(.val(seg2), .seg(hex2));
+
+//These displays stay the same between the two dates
 sevenSeg S0(.val(4'b0001), .seg(hex0));
 sevenSeg S1(.val(4'b0000), .seg(hex1));
-sevenSeg S2(.val(seg2), .seg(hex2));
 sevenSeg S3(.val(4'b0010), .seg(hex3));
 sevenSeg S4(.val(4'b1000), .seg(hex4));
 sevenSeg S5(.val(4'b0000), .seg(hex5));
 
 
-//Changing the values that need to be changed
+//Changing the displays that need to be changed based on key1
 always @(key[1])
 begin
   if(key[1])
--- Project 1/top.v
+++ Project 1/top.v
@@ -10,13 +10,6 @@ output [7:0] HEX3;
 output [7:0] HEX4;
 output [7:0] HEX5;
 
-//assign HEX0 = 8'b11111001;
-//assign HEX1 = SW;
-//assign HEX2 = 8'b10110000;
-//assign HEX2 = 8'b10011001;
-//assign HEX3 = 8'b10100100;
-//assign HEX4 = 8'b10000000;
-//assign HEX5 = 8'b11000000;
 
 design1 U0(.switch(SW), .key(KEY), .leds(LEDR),.hex0(HEX0),.hex1(HEX1),.hex2(HEX2),.hex3(HEX3),.hex4(HEX4),.hex5(HEX5));
 
