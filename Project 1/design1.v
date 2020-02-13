a131ecc36232c0fac5b5e7a79aa9aaf20685c7fd
Matt Hartnett <matthew.e.hartnett@gmail.com>
2020-02-13 08:38:44 -0700

    Finished the code of design 1 (birthday display and switch toggled leds)

--- Project 1/design1.v
+++ Project 1/design1.v
@@ -1,12 +1,38 @@
-module design1 (input [7:0] switch, input y, output [7:0] leds,output [7:0] hex0,output [7:0] hex1,output [7:0] hex2,output [7:0] hex3,output [7:0] hex4,output [7:0] hex5);
+module design1 (input [7:0] switch, input [1:0] key, output reg [9:0] leds,output [7:0] hex0,output [7:0] hex1,output [7:0] hex2,output [7:0] hex3,output [7:0] hex4,output [7:0] hex5);
 
-assign leds = ~switch;
+always @(key[0])
+begin
+  if(key[0])
+  begin
+    leds[7:0] = switch;
+  end
+  else
+  begin
+    leds[7:0] = ~switch;
+  end
+end
 
+//Displays that are the same between our birthdays
+reg [3:0] seg2 = 4'b0011;
 sevenSeg S0(.val(4'b0001), .seg(hex0));
 sevenSeg S1(.val(4'b0000), .seg(hex1));
-sevenSeg S2(.val(4'b0011), .seg(hex2));
+sevenSeg S2(.val(seg2), .seg(hex2));
 sevenSeg S3(.val(4'b0010), .seg(hex3));
 sevenSeg S4(.val(4'b1000), .seg(hex4));
 sevenSeg S5(.val(4'b0000), .seg(hex5));
+
+
+//Changing the values that need to be changed
+always @(key[1])
+begin
+  if(key[1])
+  begin
+    seg2 = 4'b0011;
+  end
+  else
+  begin
+    seg2 = 4'b0100;
+  end
+end
 
 endmodule
--- Project 1/top.v
+++ Project 1/top.v
@@ -1,4 +1,15 @@
-module top (input [7:0] SW, input [1:0] KEY, output [7:0] LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+module top (SW, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+
+input [7:0] SW;
+input [1:0] KEY;
+output [9:0] LEDR;
+output [7:0] HEX0;
+output [7:0] HEX1;
+output [7:0] HEX2;
+output [7:0] HEX3;
+output [7:0] HEX4;
+output [7:0] HEX5;
+
 //assign HEX0 = 8'b11111001;
 //assign HEX1 = SW;
 //assign HEX2 = 8'b10110000;
@@ -7,6 +18,6 @@ module top (input [7:0] SW, input [1:0] KEY, output [7:0] LEDR, HEX0, HEX1, HEX2
 //assign HEX4 = 8'b10000000;
 //assign HEX5 = 8'b11000000;
 
-design1 U0(.switch(SW), .y(1'b1), .leds(LEDR),.hex0(HEX0),.hex1(HEX1),.hex2(HEX2),.hex3(HEX3),.hex4(HEX4),.hex5(HEX5));
+design1 U0(.switch(SW), .key(KEY), .leds(LEDR),.hex0(HEX0),.hex1(HEX1),.hex2(HEX2),.hex3(HEX3),.hex4(HEX4),.hex5(HEX5));
 
 endmodule
